48cdafd418caa1ed21d2e4e04e94511bd5c0ffce
dhyaa <mohkerrouche9@gmail.com>
2026-08-01 01:47:45 +0100

    finishing get task controller and next is  update and delete task controller

--- server/src/controllers/taskController.js
+++ server/src/controllers/taskController.js
@@ -1,6 +1,7 @@
 const Task = require("../models/Task");
 const Project = require("../models/Project");
 const allowedPriorities = ["low", "medium", "high"];
+const allowedStatuses = ["todo", "in-progress", "done"];
 
 const createTask = async (req, res) => {
 
@@ -28,13 +29,14 @@ const createTask = async (req, res) => {
 
     try {
 
+        const { projectId } = req.params;
+        
         const existingProject = await Project.findOne({
-            _id: req.params.projectId,
+            _id: projectId,
             owner: req.user.id
         }).exec();
         if (!existingProject) return res.status(404).json({ message: "Project not found" });
 
-        const { projectId } = req.params;
         const existingTask = await Task.findOne({
             title,
             project: projectId
@@ -46,7 +48,7 @@ const createTask = async (req, res) => {
             description,
             priority,
             dueDate,
-            project: req.params.projectId,
+            project: projectId
         });
         return res.status(201).json(task);
     } catch (err) {
@@ -55,4 +57,85 @@ const createTask = async (req, res) => {
 
 
 }
-module.exports = { createTask }
+
+const getAllTasks = async (req, res) => {
+
+    try {
+        
+        
+        const projectId = req.params.projectId;
+        const limit = parseInt(req.query.limit) || 10;
+        const page = parseInt(req.query.page) || 1;
+        
+        if (page < 1 || limit < 1 || limit > 100) {
+            return res.status(400).json({
+                message: "Invalid pagination values"
+            });
+        }
+        
+        const project = await Project.findOne({
+            _id: projectId,
+            owner: req.user.id
+        }).exec();
+        if(!project) return res.status(404).json({ message: "Project not found" });
+        
+        const skip = (page - 1) * limit;
+
+        let filter = { project: projectId };
+
+        if (
+            req.query.status &&
+            !allowedStatuses.includes(req.query.status)
+        ) {
+            return res.status(400).json({
+                message: "Invalid status."
+            });
+        }
+
+        if (req.query.status) {
+            filter.status = req.query.status;
+        }
+
+        const tasks = await Task.find(filter)
+            .skip(skip)
+            .limit(limit)
+            .sort({ createdAt: -1 });
+
+        const totalTasks = await Task.countDocuments(filter);
+
+        const totalPages = Math.ceil(totalTasks / limit);
+
+        return res.status(200).json({
+            tasks,
+            currentPage: page,
+            totalPages,
+            totalTasks,
+            limit
+        });
+    } catch (error) {
+        return res.status(500).json({ message: error.message });
+    }
+
+}
+
+const getTaskById = async (req, res) => {
+    const {taskId} = req.params
+
+    try { 
+
+        const task = await Task.findOne(taskId).exec();
+        if (!task) return res.status(404).json({ message: "Task not found" });
+        
+        const project = await Project.findOne({
+            _id: task.project,
+            owner: req.user.id
+        }).exec();
+        if (!project) return res.status(403).json({ message: "forbidden" });
+
+        return res.status(200).json(task);
+    } catch (error) {
+        return res.status(500).json({ message: error.message });
+    }
+}
+
+module.exports = { createTask, getAllTasks }
