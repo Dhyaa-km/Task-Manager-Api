cc3f08214f73caabe51ba00d69f71caaf8ecb108
dhyaa <mohkerrouche9@gmail.com>
2026-08-11 12:13:48 +0100

    test routes with postman and fix the bougs if there exist

--- server/src/server.js
+++ server/src/server.js
@@ -2,6 +2,8 @@ require("dotenv").config();
 
 const express = require("express");
 const cors = require("cors");
+const cookieParser = require("cookie-parser");
+
 
 const connectDB = require("./config/dbConn");
 
@@ -18,6 +20,7 @@ const PORT = process.env.PORT || 3000;
 // Middleware
 app.use(cors());
 app.use(express.json());
+app.use(cookieParser());
 
 // Routes
 app.use("/api/admin", adminRoutes);
