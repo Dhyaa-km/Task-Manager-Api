9634dc2beee3a6d57178a10ce3f6ec62cb314320
dhyaa <mohkerrouche9@gmail.com>
2026-08-21 20:22:20 +0100

    fixing bugs in testing phase "postman"

--- server/src/controllers/projectController.js
+++ server/src/controllers/projectController.js
@@ -1,36 +1,54 @@
 const Project = require("../models/Project");
 
-
-const createProject = async (req, res) => { 
+const createProject = async (req, res) => {
 
     const title = req.body.title?.trim();
     const description = req.body.description?.trim();
 
     if (!title || !description) {
-        return res.status(400).json({ message: "Title and description are required." });
+        return res.status(400).json({
+            message: "Title and description are required."
+        });
     }
 
     if (title.length < 3) {
-        return res.status(400).json({ message: "Title must be at least 3 characters long." });
+        return res.status(400).json({
+            message: "Title must be at least 3 characters long."
+        });
     }
 
     if (description.length < 10) {
-        return res.status(400).json({ message: "Description must be at least 10 characters long." });
+        return res.status(400).json({
+            message: "Description must be at least 10 characters long."
+        });
     }
 
     try {
-        const project = await Project.create(
-            {
-                title,
-                description,
-                owner: req.user.id
-            }
-        );
+        const existingProject = await Project.findOne({
+            title,
+            owner: req.user.id
+        }).exec();
+
+        if (existingProject) {
+            return res.status(409).json({
+                message: "Project already exists"
+            });
+        }
+
+        const project = await Project.create({
+            title,
+            description,
+            owner: req.user.id
+        });
+
         return res.status(201).json(project);
+
     } catch (err) {
-        return res.status(500).json({ message: err.message });
+        return res.status(500).json({
+            message: err.message
+        });
     }
-}
+};
 
 const getAllProjects = async (req, res) => {
     let page = parseInt(req.query.page) || 1;
--- server/src/routes/api/taskRoutes.js
+++ server/src/routes/api/taskRoutes.js
@@ -3,12 +3,13 @@ const router = express.Router();
 
 const { createTask, getAllTasks, getTaskById, patchTaskById, deleteTaskById } = require("../../controllers/taskController");
 const { verifyJWT } = require("../../middleware/verifyJWT");
+const validateObjectId  = require("../../middleware/validateObjectId");
 
-router.post("/projects/:projectId/tasks", verifyJWT, createTask);
-router.get("/projects/:projectId/tasks", verifyJWT, getAllTasks);
+router.post("/projects/:projectId/tasks", verifyJWT, validateObjectId("projectId"), createTask);
+router.get("/projects/:projectId/tasks", verifyJWT, validateObjectId("projectId"), getAllTasks);
 // Individual task
-router.get("/tasks/:taskId", verifyJWT, getTaskById);
-router.patch("/tasks/:taskId", verifyJWT, patchTaskById);
-router.delete("/tasks/:taskId", verifyJWT, deleteTaskById);
+router.get("/tasks/:taskId", verifyJWT, validateObjectId("taskId"), getTaskById);
+router.patch("/tasks/:taskId", verifyJWT, validateObjectId("taskId"), patchTaskById);
+router.delete("/tasks/:taskId", verifyJWT, validateObjectId("taskId"), deleteTaskById);
 
 module.exports = router;
