e0f41da76978ac784d7586a8bf4c21ceb2dfd8c4
dhyaa <mohkerrouche9@gmail.com>
2026-09-08 22:21:04 +0100

    setup for deployment

--- server/src/server.js
+++ server/src/server.js
@@ -20,7 +20,7 @@ const PORT = process.env.PORT || 3000;
 // Middleware
 app.use(
     cors({
-        origin: "http://localhost:5173",
+        origin: process.env.CLIENT_URL,
         credentials: true,
     })
 );
