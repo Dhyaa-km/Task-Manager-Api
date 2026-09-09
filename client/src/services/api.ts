9af886bdcc72650ff045711f871417f611afb209
dhyaa <mohkerrouche9@gmail.com>
2026-09-09 13:34:08 +0100

    fix vercel deployment issue in router

--- client/src/services/api.ts
+++ client/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://task-manager-api-8ma3.onrender.com/api",
+  baseURL: import.meta.env.VITE_API_URL,
   withCredentials: true,
 });
 
